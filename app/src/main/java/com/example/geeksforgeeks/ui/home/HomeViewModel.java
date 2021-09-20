086447dcf1cf952d3878287f283a2b58c6e3dbf5
Joseph Moshood <gladstargtschool6@gmail.com>
2021-09-20 18:07:29 +0100

    Update HomeViewModel.java

--- app/src/main/java/com/example/geeksforgeeks/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/geeksforgeeks/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Gladstar Gifted and Talented School");
     }
 
     public LiveData<String> getText() {
